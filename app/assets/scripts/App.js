acab4b33848d2ca3ca6dcb9059fadb7b62b1dcb3
ZNG <narangerel.z@mod.gov.mn>
2026-04-06 08:54:00 +0800

    login page

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,4 @@
+import "../styles/styles.css";
+import "./in-Page";
+
+// alert("тест хийж бна...");
---
+++ app/assets/scripts/in-Page.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn_login");
+
+btn.addEventListener("click", function () {
+  // Change 'target-page.html' to your actual file name or URL
+  window.location.href = "/app/assets/pages/menu.html";
+});
